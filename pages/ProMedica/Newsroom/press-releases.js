6ebbc34631950d4c849b931094b1c65e99a0caaf
Número Cuatro <numerocuatro@pm.me>
2023-02-07 19:26:54 -0500

    fix: Sanitize Press Releases

--- pages/ProMedica/Newsroom/press-releases.js
+++ pages/ProMedica/Newsroom/press-releases.js
@@ -1,12 +1,13 @@
 import fs from 'fs';
 import { chromium } from 'playwright';
+import sanitize from '../../../lib/sanitize.js';
 
 export default async function Pressreleases() {
   const browser = await chromium.launch({ headless: true });
   const page = await browser.newPage();
 
   await page.goto('https://www.promedica.org/newsroom/press-releases/?', {
-    waitUntil: 'networkidle',
+    waitUntil: 'domcontentloaded',
   });
 
   await page.waitForSelector("a[aria-label='Next']");
@@ -32,12 +33,12 @@ export default async function Pressreleases() {
 
       const articlesPerPage = await page.$$eval('.ih-item', (headerArticle) => {
         return headerArticle.map((article) => {
-          const title = article.querySelector('.row div h2.ih-title').innerText;
-          const description = article.querySelector('.row div p').innerText;
-          const linkSrc = article.querySelector('.row div a:not(.hidden)').href;
-          const linkText = article.querySelector('.row div a:not(.hidden)').innerText;
-          const imgSrc = article.querySelector('.row div img:not(.hidden)').src;
-          const imgAlt = article.querySelector('.row div img:not(.hidden)').alt;
+          const title = article.querySelector('.row div h2.ih-title')?.innerText || null;
+          const description = article.querySelector('.row div p')?.innerText || null;
+          const linkSrc = article.querySelector('.row div a:not(.hidden)')?.href || null;
+          const linkText = article.querySelector('.row div a:not(.hidden)')?.innerText || null;
+          const imgSrc = article.querySelector('.row div img:not(.hidden)')?.src || null;
+          const imgAlt = article.querySelector('.row div img:not(.hidden)')?.alt || null;
 
           return {
             title,
@@ -64,27 +65,20 @@ export default async function Pressreleases() {
 
   const mergeItems = articles.flat().map((item, index) => ({ id: index + 1, ...item }));
   const jsonContent = JSON.stringify(mergeItems, null, 2);
-  fs.writeFile(
-    './json/ProMedica/newsroom/Press Releases/press-releases.json',
-    jsonContent,
-    'utf8',
-    function (err) {
-      if (err) return console.log(err);
-      console.log('\nPress Releases Imported!\n');
-    }
-  );
+  fs.writeFile('./json/ProMedica/newsroom/Press Releases/press-releases.json', jsonContent, 'utf8', function (err) {
+    if (err) return console.log(err);
+    console.log('\nPress Releases Imported!\n');
+  });
 
   // Press Release content
   const mergeLinks = mergeItems.map((item) => {
-    if (
-      item.linkSrc.startsWith('https://www.promedica.org/') &&
-      item.linkSrc !== 'https://www.promedica.org/newsroom/press-releases/?'
-    ) {
+    if (item.linkSrc.startsWith('https://www.promedica.org/') && item.linkSrc !== 'https://www.promedica.org/newsroom/press-releases/?') {
       return item.linkSrc;
     }
   });
 
   let articlesBody = [];
+  let articleImages = [];
   for (let i = 0; i <= mergeLinks.length; i++) {
     if (mergeLinks[i] !== undefined) {
       await page.goto(mergeLinks[i], { waitUntil: 'domcontentloaded' });
@@ -93,16 +87,19 @@ export default async function Pressreleases() {
         await page.waitForSelector('.ih-content-column');
 
         const articlesTitle = await page.title();
-        const articleContent = await page.$eval(
-          '.ih-content-column',
-          (i) => i.querySelector('#ih-page-body').innerHTML
-        );
+        const articleContent = await page.$eval('.ih-content-column', (i) => i.querySelector('#ih-page-body')?.innerHTML);
 
         articlesBody.push({
           id: i + 1,
           title: articlesTitle,
           url: mergeLinks[i],
-          content: articleContent,
+          content: sanitize(articleContent),
+        });
+
+        const allImg = await page.$$eval('.ih-content-column img', (img) => img.map((i) => i.src));
+        articleImages.push({
+          article: articlesTitle,
+          images: allImg,
         });
 
         console.log('Press Releases Article', i + 1, 'Done');
@@ -113,15 +110,16 @@ export default async function Pressreleases() {
   }
 
   const jsonArticlesContent = JSON.stringify(articlesBody, null, 2);
-  fs.writeFile(
-    './json/ProMedica/newsroom/Press Releases/press-releases-articles.json',
-    jsonArticlesContent,
-    'utf8',
-    (err) => {
-      if (err) return console.log(err);
-      console.log('\nPress Releases Articles Imported!\n');
-    }
-  );
+  fs.writeFile('./json/ProMedica/newsroom/Press Releases/press-releases-articles.json', jsonArticlesContent, 'utf8', (err) => {
+    if (err) return console.log(err);
+    console.log('Press Releases Articles Imported!\n');
+  });
+
+  const jsonArticlesImages = JSON.stringify(articleImages, null, 2);
+  fs.writeFile('./json/ProMedica/newsroom/Press Releases/press-releases-images.json', jsonArticlesImages, 'utf8', (err) => {
+    if (err) return console.log(err);
+    console.log("\nPress Releases Articles' Images Imported!\n");
+  });
 
   await page.close();
   await browser.close();
