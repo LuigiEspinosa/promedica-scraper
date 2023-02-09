492235e6c967642bfa51ff8446787dae9350ddc0
Número Cuatro <numerocuatro@pm.me>
2023-02-08 19:29:22 -0500

    fix: Sanitize ProMedica News

--- pages/ProMedica/Newsroom/news.js
+++ pages/ProMedica/Newsroom/news.js
@@ -1,11 +1,12 @@
 import fs from 'fs';
 import { chromium } from 'playwright';
+import sanitize from '../../../lib/sanitize.js';
 
 export default async function News() {
-  const browser = await chromium.launch({ headless: true });
+  const browser = await chromium.launch({ headless: false });
   const page = await browser.newPage();
 
-  await page.goto('https://www.promedica.org/newsroom/news/?', { waitUntil: 'networkidle' });
+  await page.goto('https://www.promedica.org/newsroom/news/?', { waitUntil: 'domcontentloaded' });
 
   await page.waitForSelector("a[aria-label='Next']");
   await page.click("a[aria-label='Next']");
@@ -30,12 +31,12 @@ export default async function News() {
 
       const articlesPerPage = await page.$$eval('.ih-item', (headerArticle) => {
         return headerArticle.map((article) => {
-          const title = article.querySelector('.row div h2.ih-title').innerText;
-          const description = article.querySelector('.row div p').innerText;
-          const linkSrc = article.querySelector('.row div a:not(.hidden)').href;
-          const linkText = article.querySelector('.row div a:not(.hidden)').innerText;
-          const imgSrc = article.querySelector('.row div img:not(.hidden)').src;
-          const imgAlt = article.querySelector('.row div img:not(.hidden)').alt;
+          const title = article.querySelector('.row div h2.ih-title')?.innerText || null;
+          const description = article.querySelector('.row div p')?.innerText || null;
+          const linkSrc = article.querySelector('.row div a:not(.hidden)')?.href || null;
+          const linkText = article.querySelector('.row div a:not(.hidden)')?.innerText || null;
+          const imgSrc = article.querySelector('.row div img:not(.hidden)')?.src || null;
+          const imgAlt = article.querySelector('.row div img:not(.hidden)')?.alt || null;
 
           return {
             title,
@@ -69,34 +70,54 @@ export default async function News() {
 
   // ProMedica News content
   const mergeLinks = mergeItems.map((item) => {
-    if (
-      item.linkSrc.startsWith('https://www.promedica.org/') &&
-      item.linkSrc !== 'https://www.promedica.org/newsroom/news/?'
-    ) {
+    if (item.linkSrc.startsWith('https://www.promedica.org/') && item.linkSrc !== 'https://www.promedica.org/newsroom/news/?') {
       return item.linkSrc;
     }
   });
 
   let newsBody = [];
+  let newsImages = [];
   for (let i = 0; i <= mergeLinks.length; i++) {
     if (mergeLinks[i] !== undefined) {
       await page.goto(mergeLinks[i], { waitUntil: 'domcontentloaded' });
 
       try {
+        let newsTitle = await page.title();
+
+        // * 403 ERROR - Uncomment if necessary
+        // function delay(time) {
+        //   return new Promise(function (resolve) {
+        //     setTimeout(resolve, time);
+        //   });
+        // }
+
+        // while (newsTitle.includes('ERROR')) {
+        //   let wait = 40000;
+
+        //   console.log('403 ERROR DETECTED');
+        //   await delay(wait);
+        //   wait = wait * 2;
+
+        //   console.log('RELOADING PAGE');
+        //   await page.reload();
+        //   newsTitle = await page.title();
+        // }
+
         await page.waitForSelector('.ih-content-column');
 
-        const newsTitle = await page.title();
-        const newsContent = await page.$eval('.ih-content-column', (i) => {
-          let content = i.querySelector('#ih-page-body');
-          if (content) content = content.innerHTML;
-          return content;
-        });
+        const newsContent = await page.$eval('.ih-content-column', (i) => i.querySelector('#ih-page-body')?.innerHTML || null);
 
         newsBody.push({
           id: i + 1,
           title: newsTitle,
           url: mergeLinks[i],
-          content: newsContent,
+          content: sanitize(newsContent),
+        });
+
+        const allImg = await page.$$eval('.ih-content-column img', (img) => img.map((i) => i.src));
+        newsImages.push({
+          article: newsTitle,
+          images: allImg,
         });
 
         console.log('News Articles', i + 1, 'Done');
@@ -107,15 +128,16 @@ export default async function News() {
   }
 
   const jsonNewsContent = JSON.stringify(newsBody, null, 2);
-  fs.writeFile(
-    './json/ProMedica/newsroom/News/news-articles.json',
-    jsonNewsContent,
-    'utf8',
-    (err) => {
-      if (err) return console.log(err);
-      console.log('\nNews Articles Imported!\n');
-    }
-  );
+  fs.writeFile('./json/ProMedica/newsroom/News/news-articles.json', jsonNewsContent, 'utf8', (err) => {
+    if (err) return console.log(err);
+    console.log('\nNews Articles Imported!\n');
+  });
+
+  const jsonNewsImages = JSON.stringify(newsImages, null, 2);
+  fs.writeFile('./json/ProMedica/newsroom/News/news-articles-images.json', jsonNewsImages, 'utf8', (err) => {
+    if (err) return console.log(err);
+    console.log("\nNews Articles' Images Imported!\n");
+  });
 
   await page.close();
   await browser.close();
